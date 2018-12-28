f700dba32d638a87a7f8b08ca3740f5d903fe9f2
amitsrivastava4all <amitsrivastava4all@gmail.com>
2018-12-28 16:10:57 +0530

    Code

---
+++ Code2/CrudApp.java
@@ -0,0 +1,182 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+class SortByName implements Comparator<Customer>{
+	@Override
+	public int compare(Customer first, Customer second){
+		return first.getName().compareToIgnoreCase(second.getName());
+	}
+}
+
+class CustomerOperations{
+	private static ArrayList<Customer> customers = new ArrayList<>();
+	static long totalId(){
+		return customers.stream().
+		filter(customer->customer.getId()>2000)
+		.mapToInt(customer->customer.getId()).sum();
+//		long sum =0;
+//		for(Customer customer : customers){
+//			if(customer.getId()>2000)
+//			sum+=customer.getId();
+//		}
+//		return sum;
+	}
+	static void addCustomer(int id , String name , String city){
+		Customer customer = new Customer(id, name, city);
+		customers.add(customer);
+	}
+	static ArrayList<Customer> getCustomers(){
+		return customers;
+	}
+	static String search(int id , String name , String city){
+		Customer searchCustomer = new Customer(id , name, city);
+		return customers.contains(searchCustomer)?"Found ":"Not Found";
+	}
+	static void sort(){
+		//Collections.sort(customers); // trigger Comparable
+		//Collections.sort(customers,new SortByName());  // trigger Comparator
+		/*Collections.sort(customers,new Comparator<Customer>(){
+			public int compare(Customer first, Customer second){
+				return first.getCity().compareToIgnoreCase(second.getCity());
+			}
+		});*/
+		Collections.sort(customers,
+				(first,second)->((Integer)first.getId()).compareTo(second.getId()));
+		Collections.sort(customers,
+				(first,second)->first.getName().compareToIgnoreCase(second.getName()));
+	}
+	static String delete(int id ){
+		Customer searchCustomer = new Customer(id);
+		int index = customers.indexOf(searchCustomer);
+		if(index>=0){
+			customers.remove(index);
+			return "Record Deleted";
+		}
+		else{
+			return "Can't Delete Record Not Found";
+		}
+		//return index>=0?"Record Delete ":"Can't Delete Not Found";
+	}
+}
+class Customer implements Comparable<Customer>{
+	private int id;
+	private String name;
+	private String city;
+	
+	@Override
+	public int compareTo(Customer object){
+			//return this.name.compareToIgnoreCase(object.name);
+		//return object.name.compareToIgnoreCase(this.name);
+		return ((Integer)this.id).compareTo(object.id);
+	}
+	Customer(int id){
+		this.id = id;
+	}
+	Customer(int id, String name, String city){
+		this.id = id;
+		this.name = name;
+		this.city = city;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public boolean equals(Object object){
+		if(this==object){
+			return true;
+		}
+		if(object instanceof Customer){
+			Customer customer = (Customer) object;
+			if(this.id == customer.id && this.name==null){
+				return true;
+			}
+			if(this.id==customer.id && this.name.equals(customer.name) && this.city.equals(customer.city)){
+				return true;
+			}
+		}
+		return false;
+	}
+	@Override
+	public String toString(){
+		return "id "+id+ "name "+name+" City "+city+"\n";
+	}
+	
+}
+public class CrudApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		while(true){
+			System.out.println("1. Add a Customer");
+			System.out.println("2. Search a Customer");
+			System.out.println("3. Delete a Customer");
+			System.out.println("4. Update a Customer");
+			System.out.println("5. Sort  Customers");
+			System.out.println("6. Count Customers");
+			System.out.println("7. Total Balance of All Customers");
+			System.out.println("8 . Print");
+			System.out.println("9 . exit");
+			int choice = s.nextInt();
+			switch(choice){
+			case 7:
+				
+			case 6:
+				System.out.println("Total "+CustomerOperations.getCustomers().size());
+				break;
+			case 1:
+				System.out.println("Enter the Id ");
+				int id = s.nextInt();
+				System.out.println("Enter the Name");
+				String name = s.next();
+				System.out.println("Enter the City");
+				String city = s.next();
+				CustomerOperations.addCustomer(id, name, city);
+				break;
+			case 2:
+				System.out.println("Enter the Id ");
+				int id1 = s.nextInt();
+				System.out.println("Enter the Name");
+				String name1 = s.next();
+				System.out.println("Enter the City");
+				String city1 = s.next();
+				System.out.println(CustomerOperations.search(id1, name1, city1));
+				break;
+			case 3:
+				System.out.println("Enter the Id ");
+				int id2 = s.nextInt();
+				System.out.println(CustomerOperations.delete(id2));
+				break;
+			case 5:
+				CustomerOperations.sort();
+				System.out.println(CustomerOperations.getCustomers());
+				break;
+			case 8:
+				System.out.println(CustomerOperations.getCustomers());
+				break;
+			}
+			
+		}
+
+	}
+
+}
---
+++ Code2/Demo2.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.TreeSet;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet<String> songs = new TreeSet<>();
+		songs.add("It’s My Life"); 
+		songs.add("It’s My Life");
+		songs.add("Party All Night");
+		songs.add("Party All Night");
+		System.out.println(songs);
+	}
+
+}
